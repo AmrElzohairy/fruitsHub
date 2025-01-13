5a5732632391224073cfe5aadfa845afba1fe4c0
amrElzohairy <aelzohairy9@gmail.com>
2025-01-13 12:33:17 +0200

    add fire store to app an create data base class and fir store class

---
+++ lib/core/services/data_base_services.dart
@@ -0,0 +1,4 @@
+abstract class DataBaseServices {
+
+  Future<void> addData({required String path ,Map<String, dynamic> data});
+}
---
+++ lib/core/services/fire_store_services.dart
@@ -0,0 +1,9 @@
+import 'package:fruits/core/services/data_base_services.dart';
+
+class FireStoreServices implements DataBaseServices {
+  @override
+  Future<void> addData({required String path,required Map<String, dynamic> data}) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+}
